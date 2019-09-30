f33124e01aac9bedeacbec07f8870b5501461e18
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-30 16:28:21 -0400

    adds secrets file

---
+++ Persistence-FileManager/Persistence-FileManager/Supporting Files/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  Persistence-FileManager
+//
+//  Created by Sunni Tang on 9/30/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    static let apiKey = "13797421-6b5e1cbd35ea35fde913ee48e"
+}
